b8ed281010deb9b991756f677aae8acdd3b5997f
aminbabu12 <badrulaminbabu@gmail.com>
2024-12-03 15:41:08 +0600

    6th commit

--- remove_contact.py
+++ remove_contact.py
@@ -1,12 +1,17 @@
 import save_contact
 
 def delete_contact(contacts):
-  number = int(input('Enter number which you want to remove: '))
-  for cbook in contacts:
-    if cbook['number'] == number:
-      contacts.remove(cbook)
-      save_contact.save_file(contacts)
-      print('Book removed Successfully')
+  if contacts != []:
+    try:
+      number = int(input('Enter number which you want to remove : '))
+    except ValueError:
+      print('The phone number must be an integer')
       return contacts
+    for cbook in contacts:
+      if cbook['number'] == number:
+        contacts.remove(cbook)
+        save_contact.save_file(contacts)
+        print('Book removed Successfully')
+        return contacts
   print('Empty Contact Book')
   return contacts
--- search_book.py
+++ search_book.py
@@ -1,6 +1,10 @@
 def find_book(contact_book):
   if contact_book != []:
-    number = int(input('Enter book number which you want to search: '))
+    try:
+      number = int(input('Enter book number which you want to search: '))
+    except ValueError:
+      print('The phone number must be an integer')
+      return contact_book
     found = False
     
     for cbook in contact_book:
@@ -13,48 +17,3 @@ def find_book(contact_book):
   
   else:
     print('Empty Contact Book')
-
-
-
-
-
-# def find_book(contact_book):
-#     if contact_book != []:  # Check if the contact book is not empty
-#         number = int(input('Enter book number which you want to search: '))
-#         found = False  # Flag to track if the number is found
-
-#         for cbook in contact_book:
-#             if cbook['number'] == number:
-#                 found = True
-#                 print(f"Here is the contact book:\n"
-#                       f"Name: {cbook['name']} | Number: {cbook['number']} | "
-#                       f"Email: {cbook['email']} | Address: {cbook['address']}")
-#                 break  # Exit the loop as we've found the contact book
-
-#         if not found:
-#             print('No Book Found')  # Message when the number is not found
-#     else:
-#         print('Empty Contact Book')  # Message when the contact book is empty
-
-
-
-
-
-
-
-
-
-
-
-
-"""
-def find_book(contact_book):
-  if contact_book !=  []:
-    number = int(input('Enter book number which you want to search : '))
-    for cbook in contact_book:
-      if cbook['number'] == number:
-        print(f"Here is the contact book:\nName: {cbook['name']} | Number: {cbook['number']} | Email: {cbook['email']} | Adress: {cbook['address']}")
-
-  else:
-    print('Empty Contact Book')
-"""
